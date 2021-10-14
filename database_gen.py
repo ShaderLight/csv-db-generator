25a118719d8bfce2f79e1594a3790000844532dc
ShaderLight <37264318+ShaderLight@users.noreply.github.com>
2021-10-14 10:43:31 +0200

    db generation

--- database_gen.py
+++ database_gen.py
@@ -1,5 +1,25 @@
-import modules.input_classes
-from modules.csv_io import csv_to_list
+from modules.input_classes import*
+from modules.csv_io import list_to_csv
+
 
 def generate_db(output_filename, n, *args):
-    pass
+    output = []
+    fieldnames = []
+
+    for field_class in args:
+        try:
+            fieldnames.append(field_class.name)
+        except AttributeError:
+            fieldnames.extend(field_class.names)
+    
+    temp_val = []
+    for i in range(len(args)):
+        if i == 0:
+            output.extend(args[0].generate(n))
+            continue
+        temp_val = args[i].generate(n)
+
+        for k in range(n):
+            output[k].update(temp_val[k])
+
+    return list_to_csv(output_filename, output, fieldnames)
--- modules/csv_io.py
+++ modules/csv_io.py
@@ -1,8 +1,8 @@
-import csv
+from csv import DictReader, DictWriter
 
 def csv_to_list(filename):
     with open(filename, mode='r', encoding='utf-8') as f:
-        csv_reader = csv.DictReader(f, delimiter=';')
+        csv_reader = DictReader(f, delimiter=';')
         output = []
         for row in csv_reader:
             output.append(row)
@@ -11,11 +11,14 @@ def csv_to_list(filename):
 
 def list_to_csv(filename, input_list, fieldnames):
     with open(filename, mode='w', newline='', encoding='utf-8') as f:
-        csv_writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter=';')
+        csv_writer = DictWriter(f, fieldnames=fieldnames, delimiter=';')
 
         csv_writer.writeheader()
 
+        print(fieldnames)
+        print(input_list)
         for row in input_list:
+            print(row)
             csv_writer.writerow(row)
 
     return 0
